4730a3a20bca102d1ba633523f6b595ae677c965
Durjoy Das <durjoydas2122@gmail.com.com>
2026-06-10 02:31:03 +0530

    Use env vars for Google Drive auth

--- backend/services/googleDrive.js
+++ backend/services/googleDrive.js
@@ -2,7 +2,14 @@ const fs = require("fs");
 const { google } = require("googleapis");
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: "./backend/config/google-drive-key.json",
+  credentials: {
+    type: "service_account",
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+  },
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
 
